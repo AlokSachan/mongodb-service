a6872a4fc1460b6772d7d91c9b36c56141a83be6
AlokSachan <32594923+AlokSachan@users.noreply.github.com>
2021-08-01 11:20:44 +0530

    Update CustomException.java

--- src/main/java/com/alok/exception/CustomException.java
+++ src/main/java/com/alok/exception/CustomException.java
@@ -1,1 +1,32 @@
     
+public class CustomException extends RuntimeException {
+
+    private final HttpStatus httpStatus;
+    private final ErrorDetailsTO errorDetailsTO;
+
+    public CustomException(String message, HttpStatus httpStatus, ErrorDetailsTO errorDetailsTO) {
+        super(message);
+        this.httpStatus = httpStatus;
+        this.errorDetailsTO = errorDetailsTO;
+    }
+
+    public CustomException(String message, HttpStatus httpStatus, List<ErrorDetails> issueListTO) {
+        super(message);
+        final ErrorDetailsTO tos = new ErrorDetailsTO();
+        tos.setIssues(issueListTO);
+        this.errorDetailsTO = tos;
+        this.httpStatus = httpStatus;
+    }
+
+    public ErrorDetailsTO getErrorDetailsTO() {
+        return errorDetailsTO;
+    }
+
+    public List<ErrorDetails> getIssues() {
+        return errorDetailsTO != null ? errorDetailsTO.getIssues() : Collections.emptyList();
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+}
